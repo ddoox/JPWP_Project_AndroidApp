987554fcabb0f9bbb705b88135f6604363f99bc6
dudek746@gmail.com <zaq1@WSXdinodino>
2019-05-01 12:50:43 +0200

    Uzyskiwanie najnowszej pozycji, ale spam nowymi markerami

--- app/src/main/java/com/example/jpwp_git/MapsActivity.java
+++ app/src/main/java/com/example/jpwp_git/MapsActivity.java
@@ -1,48 +1,46 @@
 package com.example.jpwp_git;
 
 import android.Manifest;
-import android.content.IntentSender;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationManager;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     //do map
     private GoogleMap mMap;
-    Location Pozycja;
+    Location PozycjaLocation;
     private FusedLocationProviderClient fusedLocationClient;
     private LocationRequest locationRequest;
+    LocationCallback locationCallback;
+    Marker AktualnaPozycjaMarker;
+    LatLng AktualnaPozycjaWspolrzedneLatLang;
+    CameraPosition PozycjaKamery;
 
     //layout
     TextView PoleTekstowe1;
@@ -51,34 +49,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Button Guzik;
 
 
-
-    private void refresh() {
-
-        //trzeba sprawdzić czy jest uprawnienie do lokalizacji
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-              return;
-        }
-
-
-        //uzyskanie ostatniej lokalizacji za pomocą API Googla
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-
-                            Pozycja = location;
-                            PoleTekstowe1.setText("Latitude  =" + Pozycja.getLatitude());
-                            PoleTekstowe2.setText("Longitude  =" + Pozycja.getLongitude());
-                            LatLng sydney = new LatLng(Pozycja.getLatitude(), Pozycja.getLongitude());
-                            mMap.addMarker(new MarkerOptions().position(sydney).title("kek"));
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-                        }
-                    }
-                });
-    } ;
 
 
     @Override
@@ -118,34 +88,122 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         });
+
+
+        //utworzenie location request potrzebnego do uzyskania aktualnej lokacji, ustawia się parametry odświeżania lokalizacji i dokładność
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+
+        //LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+        //        .addLocationRequest(locationRequest);
+
+
+        //klasa wykorzystywana do do otrzymywania aktualizacji przy zmianie położenia
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    //zmiany po ustaleniu lokacji
+                    PozycjaLocation = location;
+                    PoleTekstowe1.setText("Latitude  =" + PozycjaLocation.getLatitude());
+                    PoleTekstowe2.setText("Longitude  =" + PozycjaLocation.getLongitude());
+                    //AktualnaPozycjaMarker.setPosition(AktualnaPozycjaWspolrzedneLatLang);
+                    //AktualnaPozycjaMarker = mMap.addMarker(new MarkerOptions().position(AktualnaPozycjaWspolrzedneLatLang).title("Marker in Sydney"));
+                    //mMap.moveCamera(CameraUpdateFactory.newLatLng(AktualnaPozycjaWspolrzedneLatLang));
+                    LatLng sydney = new LatLng(PozycjaLocation.getLatitude(), PozycjaLocation.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(sydney).title("kek"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+                }
+            }
+
+
+        };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (true) { //warunek jesli otrzymywanie lokalizacji jest wlaczone - requestingLocationUpdates
+            startLocationUpdates();
+        }
+    }
 
 
+    private void startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                null /* Looper */);
+    }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Googl
-     * e Play services and returned to the app.
-     */
+    // do obsługi klawisza
+    private void refresh() {
+
+        //trzeba sprawdzić czy jest uprawnienie do lokalizacji
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+
+        //uzyskanie ostatniej lokalizacji za pomocą API Googla
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+
+                            PozycjaLocation = location;
+                            PoleTekstowe1.setText("Latitude  =" + PozycjaLocation.getLatitude());
+                            PoleTekstowe2.setText("Longitude  =" + PozycjaLocation.getLongitude());
+                            LatLng sydney = new LatLng(PozycjaLocation.getLatitude(), PozycjaLocation.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(sydney).title("kek"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                        }
+                    }
+                });
+    } ;
+
+
+    // wywoływane po tym jak mapa jest gotowa do użytku
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 10);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+       // AktualnaPozycjaWspolrzedneLatLang = new LatLng(-34, 10);
+       // AktualnaPozycjaMarker = mMap.addMarker(new MarkerOptions().position(AktualnaPozycjaWspolrzedneLatLang).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(AktualnaPozycjaWspolrzedneLatLang));
 
     }
 
 
-    }
+}
